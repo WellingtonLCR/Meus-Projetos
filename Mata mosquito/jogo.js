af47878a2cea5efecd383bc40e2d0b9258222d57
WellingtonLCR <dabliojau@gmail.com>
2024-08-22 20:50:56 -0300

    Adicionando elemento e Style Random

--- Mata mosquito/jogo.js
+++ Mata mosquito/jogo.js
@@ -7,8 +7,48 @@ function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
 
-    console.log(altura, largura)
+    console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica() {
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX < 0 ? 0 : posicaoX
+    posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //criando o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+
+    console.log (tamanhoAleatorio())
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+
 
