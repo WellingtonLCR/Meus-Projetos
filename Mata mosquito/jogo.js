a0ab4c4cac9e94443c9614e3c7234570a11c3e6c
WellingtonLCR <dabliojau@gmail.com>
2024-08-23 20:21:59 -0300

    Aplicando a pagina Game Over

--- Mata mosquito/jogo.js
+++ Mata mosquito/jogo.js
@@ -22,7 +22,8 @@ function posicaoRandomica() {
         //console.log('elemento selecionado foi: v' + vidas)
 
         if (vidas > 3) {
-            alert('Interromper o Jogo (Game Over)')
+
+        window.location.href = 'fim_de_jogo.html'
         } else {
             document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
 
